3ec03a8a0d7ddb483266b93ad1d9f6e040ce1038
cuipaly009@163.com <cuipaly009@163.com>
2015-07-20 23:57:24 +0800

    update

--- static/js/main.js
+++ static/js/main.js
@@ -2,13 +2,14 @@ var swiper = new Swiper('.swiper-container', {
     direction: 'vertical',
     slidesPerView: 1,
     paginationClickable: true,
-    spaceBetween: 30,
+    spaceBetween: 0,
     mousewheelControl: true
 });
 
 
 swiper.on('slideChangeStart', function () {
-    console.log(swiper.activeIndex);
+    var index = swiper.activeIndex;
+    $(".swiper-slide").eq(index).find('.animation-wrap').addClass("begin")
 });
 
 $('.action_1 .button').on('tap', function () {
